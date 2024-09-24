e5f282b07065a2be956f15cb2ff7732965f5afb1
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-09-24 14:01:13 +0700

    adding some helpergrid

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ import './App.css'
 const App = () => {
   return (
    <main id='canvas-container'>
-     <Canvas camera={{position:[0,0,4]}}>
+     <Canvas camera={{position:[0,0,5]}}>
+      <gridHelper args={[10, 10, "green", "blue"]}  />
+       
           <ambientLight intensity={0.1} />
           <directionalLight color="red" position={[0, 0, 5]} />
-          <Box position={[-1.2, 0, 1]}/>
-          <Box position={[2, 0, 0]}/>
+          <Box position={[-3, 0, 1]}/>
+          <Box position={[1, 0, 0]}/>
+          <Box position={[6, 0, -1]}/>
     </Canvas>
    </main>
   )
